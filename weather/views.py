81f84147e16f88f42d9a3319efa867f7af865c47
TrippleA-Ashaba <trippleaunit@hotmail.com>
2022-12-29 02:39:22 +0300

    Implemented DRY

--- weather/views.py
+++ weather/views.py
@@ -10,6 +10,27 @@ load_dotenv()
 
 API_KEY = os.environ.get("API_KEY")
 
+# Get current system time
+time = datetime.now()
+
+
+def get_data(city):
+    r = requests.get(
+        f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={API_KEY}"
+    ).json()
+
+    if len(r) != 0:
+
+        # Get latitude and longitude of city
+        lat = r[0].get("lat")
+        lon = r[0].get("lon")
+        data = requests.get(
+            f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={API_KEY}"
+        ).json()
+        return data
+    else:
+        return False
+
 
 # Create your views here.
 
@@ -18,44 +39,13 @@ def home(request):
     # checking if city has been searched, otherwise render Use Kampala as the default city
 
     if request.method == "POST":
-        error = ""
-        data = ""
         city = request.POST.get("search")
 
-        r = requests.get(
-            f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={API_KEY}"
-        ).json()
+        data = get_data(city)
 
-        if len(r) != 0:
-
-            # Get latitude and longitude of city
-            lat = r[0].get("lat")
-            lon = r[0].get("lon")
-            data = requests.get(
-                f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={API_KEY}"
-            ).json()
-        else:
-            error = "City Does Not Exist, Ensure it is spelt correctly"
-
-        # Get current system time
-        time = datetime.now()
-
-        return render(
-            request, "weather/home.html", {"data": data, "time": time, "error": error}
-        )
+        return render(request, "weather/home.html", {"data": data, "time": time})
 
     else:
-        city = "kampala"
-        r = requests.get(
-            f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={API_KEY}"
-        ).json()
+        data = get_data("kampala")
 
-        lat = r[0].get("lat")
-        lon = r[0].get("lon")
-        data = requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={API_KEY}"
-        ).json()
-
-        # Get current system time
-        time = datetime.now()
         return render(request, "weather/home.html", {"data": data, "time": time})
