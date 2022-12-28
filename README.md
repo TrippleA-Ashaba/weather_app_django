7ca5e6b155538bfdfe9efee84f2f8e5d9575725c
TrippleA-Ashaba <trippleaunit@hotmail.com>
2022-12-28 10:38:06 +0300

    modified the home view

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 ![image](https://user-images.githubusercontent.com/102288573/209771205-6878b839-6798-4736-a093-8fd054c62b40.png)
+
 # weather_app_django
 
-
-A mobile responsive, with city search functionaliy, weather app using the [Open Weather api](https://openweathermap.org/).
+A mobile responsive, with city search functionaliy, weather app using the [Open Weather API](https://openweathermap.org/).
 
 ## How Install
 
-After forking the repository, create a virtual environment on the same level as the *django_project*
+After forking the repository, create a virtual environment on the same level as the _django_project_
 
 ```
 $ python -m venv .venv
@@ -23,7 +23,9 @@ Then install all dependencies from the requirements.txt file
 ```
 $ pip install -r requirements.txt
 ```
-Still on the django_project level, create a *.env* file, Here we shall put our API_KEY and SECRET_KEY
+
+Still on the django_project level, create a _.env_ file, Here we shall put our API_KEY and SECRET_KEY
+
 ```
 SECRET_KEY = "this is my secret key"
 API_KEY = "api_key_from_openweathermap"
@@ -32,11 +34,13 @@ API_KEY = "api_key_from_openweathermap"
 ## Start Using App
 
 You can straight away start using the weather app
+
 ```
 $ python manage.py runserver
 ```
+
 Or copy the weather app contents and use in another project.
-Then register the app in *django_project/settings.py* installed apps
+Then register the app in _django_project/settings.py_ installed apps
 
 ```diff
 INSTALLED_APPS = {
@@ -46,11 +50,10 @@ INSTALLED_APPS = {
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-   
+
     "weather.apps.WeatherConfig", #new
-    
+
 }
 ```
-
 
 ### ENJOY
--- weather/views.py
+++ weather/views.py
@@ -16,31 +16,46 @@ API_KEY = os.environ.get("API_KEY")
 
 def home(request):
     # checking if city has been searched, otherwise render Use Kampala as the default city
-    error = ""
-    data = ""
-    r = ""
-    if request.method == "GET":
-        searched_city = request.GET
-        # Add a dash on long city names
-        city = searched_city.get("search").replace(" ", "-") or "kampala"
+
+    if request.method == "POST":
+        error = ""
+        data = ""
+        city = request.POST.get("search")
 
         r = requests.get(
             f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={API_KEY}"
         ).json()
 
-    if len(r) != 0:
+        if len(r) != 0:
 
-        # Get latitude and longitude of city
+            # Get latitude and longitude of city
+            lat = r[0].get("lat")
+            lon = r[0].get("lon")
+            data = requests.get(
+                f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={API_KEY}"
+            ).json()
+        else:
+            error = "City Does Not Exist, Ensure it is spelt correctly"
+
+        # Get current system time
+        time = datetime.now()
+
+        return render(
+            request, "weather/home.html", {"data": data, "time": time, "error": error}
+        )
+
+    else:
+        city = "kampala"
+        r = requests.get(
+            f"http://api.openweathermap.org/geo/1.0/direct?q={city}&appid={API_KEY}"
+        ).json()
+
         lat = r[0].get("lat")
         lon = r[0].get("lon")
         data = requests.get(
             f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={API_KEY}"
         ).json()
-    else:
-        error = "City Does Not Exist, Ensure it is spelt correctly"
 
-    # Get current system time
-    time = datetime.now()
-    return render(
-        request, "weather/home.html", {"data": data, "time": time, "error": error}
-    )
+        # Get current system time
+        time = datetime.now()
+        return render(request, "weather/home.html", {"data": data, "time": time})
